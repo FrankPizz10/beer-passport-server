ee4615e4224efea6820cb7f915ca18a775169e97
Frank Pizzella <fpizz210@gmail.com>
2023-06-03 21:55:16 -0400

    Added more tests

--- test/DBClient/gettableinfo.test.ts
+++ test/DBClient/gettableinfo.test.ts
@@ -2,7 +2,14 @@ import { getCategories } from '../../src/DBclient/gettableinfo';
 import { categories } from '@prisma/client';
 import { getAllBeers, getBeerByCategory, getBeerById } from '../../src/DBclient/beerclient';
 import { MockContext, Context, createMockContext } from '../../context';
-import { addUser } from '../../src/DBclient/userclient';
+import {
+  addUser,
+  getLikedBeersByUserId,
+  getTriedBeersByUserId,
+  getUser,
+  getUserBeerByUserIdAndBeerId,
+  getUserBeersByUserId,
+} from '../../src/DBclient/userclient';
 import { server } from '../../src';
 
 describe('DBClient', () => {
@@ -76,6 +83,87 @@ describe('DBClient', () => {
             user_name: user.user_name,
           },
         });
+      });
+    });
+    describe('getUser', () => {
+      it('should get a user', async () => {
+        const user = await getUser('xtCCTvpArfP4ZRzrjmzAyrVmO5A2');
+        if (user) {
+          expect(user.id).toBe(1);
+          expect(user.uid).toBe('xtCCTvpArfP4ZRzrjmzAyrVmO5A2');
+          expect(user.email).toBe('f1@gmail.com');
+          expect(user.age).toBe(23);
+          expect(user.user_name).toBe('Frank_Pizz10');
+        } else {
+          fail('user is null');
+        }
+      });
+    });
+    describe('getUserBeersByUserId', () => {
+      it('should get all beers for a user', async () => {
+        const user = await getUser('I8PXIyg7RwYE0HQmVBejgf7sHtd2');
+        if (user) {
+          const userBeers = await getUserBeersByUserId(user.id);
+          expect(userBeers.length).toBeGreaterThan(7);
+          expect(userBeers[0].id).toBe(3);
+          expect(userBeers[0].beer_id).toBe(1);
+          expect(userBeers[0].user_id).toBe(2);
+          expect(userBeers[0].liked).toBe(false);
+          expect(userBeers[0].tried).toBe(false);
+        } else {
+          fail('user is null');
+        }
+      });
+    });
+    describe('getUserBeerByUserIdAndBeerId', () => {
+      it('should get all beers for a user', async () => {
+        const user = await getUser('I8PXIyg7RwYE0HQmVBejgf7sHtd2');
+        if (user) {
+          const userBeer = await getUserBeerByUserIdAndBeerId(user.id, 1);
+          if (userBeer) {
+            expect(userBeer.id).toBe(3);
+            expect(userBeer.beer_id).toBe(1);
+            expect(userBeer.user_id).toBe(2);
+            expect(userBeer.liked).toBe(false);
+            expect(userBeer.tried).toBe(false);
+          } else {
+            fail('userBeer is null');
+          }
+        } else {
+          fail('user is null');
+        }
+      });
+    });
+    describe('getTriedBeersByUserId', () => {
+      it('should get all tried beers for a user', async () => {
+        const user = await getUser('xtCCTvpArfP4ZRzrjmzAyrVmO5A2');
+        if (user) {
+          const userBeers = await getTriedBeersByUserId(user.id);
+          expect(userBeers[0].id).toBe(2);
+          expect(userBeers[0].beer_id).toBe(1);
+          expect(userBeers[0].user_id).toBe(1);
+          expect(userBeers[0].liked).toBe(false);
+          expect(userBeers[0].tried).toBe(true);
+        } else {
+          fail('user is null');
+        }
+      });
+    });
+    describe('getLikedBeersByUserId', () => {
+      it('should get all liked beers for a user', async () => {
+        const user = await getUser('xtCCTvpArfP4ZRzrjmzAyrVmO5A2');
+        if (user) {
+          const userBeers = await getLikedBeersByUserId(user.id);
+          const targetBeer = userBeers.find(b => b.beer_id === 25);
+          if (targetBeer) {
+            expect(targetBeer.beer_id).toBe(6);
+            expect(targetBeer.user_id).toBe(3);
+            expect(targetBeer.liked).toBe(true);
+            expect(targetBeer.tried).toBe(true);
+          }
+        } else {
+          throw new Error('user is null');
+        }
       });
     });
   });
